d3895a71653f3f0aa03dcb9f4b9ef96a84b06a31
pothanna <pothanna.pullolla@gmail.com>
2022-06-20 09:24:37 +0000

    react routing blogList

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,14 @@
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-info-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      alt="profile"
+      className="user-image"
+    />
+    <h1 className="name">Wade Warren</h1>
+    <p className="description ">Software Developer at UK</p>
+  </div>
+)
+export default UserInfo
